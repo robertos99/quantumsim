d821f751219fd8131da63378c5f0e6ea83931849
robertos99 <104994013+robertos99@users.noreply.github.com>
2023-08-25 13:46:26 +0200

    added cswap gate

--- src/qsim/unitarys.rs
+++ src/qsim/unitarys.rs
@@ -118,7 +118,8 @@ impl CNot {
         let control = self.control;
         let target = self.target;
         let dim = 1 << register_size; // 2^register_size
-        let mut cnot_matrix = vec![vec![T::zero(); dim]; dim];
+
+        let mut cnot_matrix = vec![vec![T::zero(); dim]; dim]; // create zeroed matrix which we fill in later
 
         for i in 0..dim {
             if (i & (1 << register_size - 1 - control)) != 0 {
@@ -143,8 +144,75 @@ impl Gate<f64> for CNot {
 
 impl Gate<Complex<f64>> for CNot {
     fn apply(&self, state_vec: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
-        let cnot = self.dynamic_cnot::<Complex<f64>>(get_amount_bits(&state_vec));
+        let bits_in_register = get_amount_bits(&state_vec);
+        assert!(self.control < bits_in_register && self.target < bits_in_register, 
+            "CNot control or target bit out of register bound. Register holds {} bits. Control was {}. Target was {}.", 
+            bits_in_register, self.control, self.target);
+        let cnot = self.dynamic_cnot::<Complex<f64>>(bits_in_register);
         multiply_matrix_vector(&cnot, &state_vec)
+    }
+}
+
+struct CSwap {
+    control: usize,
+    target_1: usize,
+    target_2: usize,
+}
+
+impl CSwap {
+    pub fn new(control: usize, target_1: usize, target_2: usize) -> Self {
+        let are_equal = control == target_1 || control == target_2 || target_1 == target_2;
+        assert!(
+            !are_equal,
+            "Control, Target_1 or Target_2 are pointing to the same bit. They are not allowed to be pointing to the same bit."
+        );
+
+        Self {
+            control,
+            target_1,
+            target_2,
+        }
+    }
+
+    fn dynamic_cswap<T: Zero + One + Clone>(&self, register_size: usize) -> Vec<Vec<T>> {
+        let control = self.control;
+        let target_1 = self.target_1;
+        let target_2 = self.target_2;
+        let dim = 1 << register_size; // 2^register_size
+
+        let mut cswap_matrix = vec![vec![T::zero(); dim]; dim]; // create zeroed matrix which we fill in later
+
+        for i in 0..dim {
+            if (i & (1 << register_size - 1 - control)) != 0 {
+                let tbit_1 = i & (1 << register_size - 1 - target_1);
+                let tbit_2 = i & (1 << register_size - 1 - target_2);
+
+                if (tbit_1 == 0 && tbit_2 != 0) || (tbit_2 == 0 && tbit_1 != 0) {
+                    let swap_mask = i ^ (1 << register_size - 1 - target_1);
+                    let swap_mask = swap_mask ^ (1 << register_size - 1 - self.target_2);
+                    cswap_matrix[i][swap_mask] = T::one();
+                } else {
+                    // if control bit is not set, state remains unchanged
+                    cswap_matrix[i][i] = T::one();
+                }
+            } else {
+                // if control bit is not set, state remains unchanged
+                cswap_matrix[i][i] = T::one();
+            }
+        }
+
+        cswap_matrix
+    }
+}
+
+impl Gate<f64> for CSwap {
+    fn apply(&self, state_vec: &Vec<f64>) -> Vec<f64> {
+        let bits_in_register = get_amount_bits(&state_vec);
+        assert!(self.control < bits_in_register && self.target < bits_in_register, 
+            "CSwap control or target_1 or target_2 bit out of register bound. Register holds {} bits. Control was {}. Target_1 was {}. Target_2 was {}.", 
+            bits_in_register, self.control, self.target_1, self.target_2);
+        let cswap = self.dynamic_cswap::<f64>(get_amount_bits(&state_vec));
+        multiply_matrix_vector(&cswap, &state_vec)
     }
 }
 
@@ -282,6 +350,155 @@ mod test {
             vec![0.0, 0.0, 1.0, 0.0],
         ];
 
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_dynamic_cswap_f64_adjacent_in_order() {
+        let cswap = CSwap::new(0, 1, 2);
+        let result = cswap.dynamic_cswap::<f64>(3);
+        let expected = vec![
+            vec![1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+        ];
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_dynamic_cswap_f64_adjacent_not_in_order() {
+        let cswap = CSwap::new(0, 2, 1);
+        let result = cswap.dynamic_cswap::<f64>(3);
+        let expected = vec![
+            vec![1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+        ];
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_dynamic_cswap_f64_non_adjacent_in_order() {
+        let cswap = CSwap::new(0, 1, 3);
+        let result = cswap.dynamic_cswap::<f64>(4);
+        //1001 -> 1100; 1011 -> 1110; 1100 -> 1001; 1110 -> 1011
+        let expected = vec![
+            vec![
+                1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+            ],
+        ];
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_dynamic_cswap_f64_non_adjacent_out_of_order() {
+        let cswap = CSwap::new(0, 3, 1); // Note the change in order here for target_1 and target_2
+        let result = cswap.dynamic_cswap::<f64>(4);
+        let expected = vec![
+            vec![
+                1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+            ],
+            vec![
+                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+            ],
+        ];
         assert_eq!(expected, result);
     }
 }
